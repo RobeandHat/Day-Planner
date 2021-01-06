8282fe63e97017f02d145f6898f378aac10322df
Matt Randolph <mttrndlph@gmail.com>
2021-01-05 19:39:06 -0500

    Added moment js, linked js script, added current date

---
+++ script.js
@@ -0,0 +1,7 @@
+//1. Display current date and time at the top of the screen
+//2. Create blocks with forms for each hour of the day
+//2.5. 1 row 3 columns
+//3. Create buttons for each block that saves the form content to local storage.
+//4. Create functions to dynamically change the classes depending upon the current time.
+
+$("#currentDay").text(moment().format("MMMM Do YYYY"));
